97d074bae11853bbf7ba36233ce9ce2421097c5e
jonathanalves25 <50221540+jonathanalves25@users.noreply.github.com>
2021-06-02 21:15:29 -0300

    Add files via upload

---
+++ projeto_biblioteca/script.js
@@ -0,0 +1,5 @@
+    let menuOpenner = document.querySelector('.menu-openner');
+    
+    menuOpenner.addEventListener('click', ()=> {
+        document.querySelector('.menu').style.display = 'block';
+    });
