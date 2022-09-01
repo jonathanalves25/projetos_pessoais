36197bf562855d93edff30c952e93dcb18a5ff60
Johnny <johnnyduff25@gmail.com>
2022-09-01 20:24:35 -0300

    adicionado colaborador

--- organo/src/componentes/Time/index.js
+++ organo/src/componentes/Time/index.js
@@ -1,9 +1,17 @@
+import Colaborador from '../Colaborador';
 import './style.css';
 
 const Time = (props) => {
   return (
     <section className="time" style={{backgroundColor: props.corSecundaria}}>
       <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map(colaborador => <Colaborador 
+          nome={colaborador.nome} 
+          cargo={colaborador.cargo} 
+          imagem={colaborador.imagem}
+        />)}
+      </div>
     </section>
   )
 }
