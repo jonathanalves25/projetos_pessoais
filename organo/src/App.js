d143e3fe4aff656ac2a9f1bc305648be02d98911
Johnny <johnnyduff25@gmail.com>
2022-08-31 22:46:09 -0300

    adicionando componente de times

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,9 +1,48 @@
 import Banner from './componentes/Banner/index';
 import Formulario from './componentes/Formulario';
 import {useState} from 'react'
+import Time from './componentes/Time';
 
 
 function App() {
+
+  const times = [
+    {
+      nome: 'Front-end', 
+      corPrimaria: '#82CFFA', 
+      corSecundaria: '#E8F8FF'
+    },
+    {
+      nome: 'Data Science', 
+      corPrimaria: '#A6D157', 
+      corSecundaria: '#F0F8E2'
+    },
+    {
+      nome: 'Programação', 
+      corPrimaria: '#57C278', 
+      corSecundaria: '#D9F7E9'
+    },
+    {
+      nome: 'DevOps', 
+      corPrimaria: '#E06B69', 
+      corSecundaria: '#FDE7E8'
+    },
+    {
+      nome: 'UX e Design', 
+      corPrimaria: '#DB6EBF', 
+      corSecundaria: '#FAE9F5'
+    },
+    {
+      nome: 'Mobile', 
+      corPrimaria: '#FFBA05', 
+      corSecundaria: '#FFF5D9'
+    },
+    {
+      nome: 'Inovacao e Gestao', 
+      corPrimaria: '#FF8A29', 
+      corSecundaria: '#FFEEDF'
+    },
+  ]
 
   const [colaboradores, setColaboradores] = useState([])
 
@@ -15,7 +54,8 @@ function App() {
       <div>
         <Banner/>
         <Formulario aoColaboradorCadastrado={aoNovoColaboradorAdicionado} />
-        
+
+        {times.map(time => <Time key={time.nome} nome={time.nome} corPrimaria={time.corPrimaria} corSecundaria={time.corSecundaria}/>)}
       </div>
   );
 }
--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -7,9 +7,9 @@ import {useState} from 'react'
 const Formulario = (props) => {
   
   const times = [
-    'Programação',
     'Front-end',
     'Data Science',
+    'Programação',
     'DevOps',
     'UX e Design',
     'Mobile',
