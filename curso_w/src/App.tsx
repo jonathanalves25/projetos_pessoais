97c710a8f91fe410513b4edde76febeef14481b3
Johnny <johnnyduff25@gmail.com>
2022-09-19 23:19:57 -0300

    configurando contextos e hooks

---
+++ curso_w/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import './App.css';
+import { Home } from './pages/Home'
+import { Private } from './pages/Private'
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Header do site</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/private">Pagina Privada</Link>
+        </nav>
+      </header>
+      <hr/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/private" element={<Private/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
