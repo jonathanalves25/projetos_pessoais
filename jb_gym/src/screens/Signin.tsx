8500d15dc8de9a900bfe9e4ade27cbd220b63130
Johnny <johnnyduff25@gmail.com>
2023-07-24 23:02:14 -0300

    configurando tela de signin

--- jb_gym/src/screens/Signin.tsx
+++ jb_gym/src/screens/Signin.tsx
@@ -1,16 +1,45 @@
-import {VStack, Image} from 'native-base';
+import {VStack, Image, Text, Center, Heading} from 'native-base';
 
+import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
+import { Input } from '@components/Input';
+
 
 export const SignIn = () => {
     return(
-        <VStack flex={1} bg="gray.700">
+        <VStack flex={1} bg="gray.700" px={10}>
             <Image 
                 source={BackgroundImg}
                 alt="Pessoas treinando na bicicleta"
                 resizeMode="contain"
                 position="absolute"
             />
+            <Center my={24}>
+                <LogoSvg/>
+                <Text color="gray.100" fontSize="sm">
+                    Treine sua mente e o seu corpo
+                </Text>
+            </Center>
+            <Center>
+                <Heading color="gray.100" mb={6} fontFamily="heading">
+                    Acesse sua conta
+                </Heading>
+
+                <Input 
+                    placeholder='E-mail'
+                    keyboardType='email-address'
+                    autoCapitalize='none'
+                />
+
+                <Input 
+                    placeholder='Senha'
+                    secureTextEntry
+                    
+                />
+            </Center>
+
+            
+
         </VStack>
     );
 }
