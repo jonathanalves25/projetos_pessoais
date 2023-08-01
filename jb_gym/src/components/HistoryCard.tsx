b27b07ea1962175796a0745b61ada0be8061d44a
Johnny <johnnyduff25@gmail.com>
2023-07-31 22:40:06 -0300

    aplicado ajustes nas interfaces

--- jb_gym/src/components/HistoryCard.tsx
+++ jb_gym/src/components/HistoryCard.tsx
@@ -3,8 +3,8 @@ import { HStack, Heading, Text, VStack } from "native-base";
 export const HistoryCard = () => {
     return (
         <HStack w="full" px={5} py={4} mb={3} bg="gray.600" rounded="md" alignItems="center" justifyContent="space-between">
-            <VStack>
-                <Heading color="white" fontSize="md" textTransform="capitalize">
+            <VStack mr={5} flex={1}>
+                <Heading color="white" fontSize="md" textTransform="capitalize" numberOfLines={1}>
                     costas
                 </Heading>
                 <Text color="gray.100" fontSize="lg" numberOfLines={1}>
--- jb_gym/src/screens/Home.tsx
+++ jb_gym/src/screens/Home.tsx
@@ -4,6 +4,8 @@ import { HomeHeader } from '@components/HomeHeader';
 import { HStack, VStack, FlatList, Heading, Text} from 'native-base';
 import {useState} from 'react';
 import { Exercise } from './Exercise';
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from '@routes/app.routes';
 
 
 export const Home = () => {
@@ -11,6 +13,12 @@ export const Home = () => {
     const [groupSelected, setGroupSelected] = useState('costas');
     const [groups, setGroups] = useState(['costas', 'ombro', 'peito', 'biceps', 'triceps'])
     const [exercises, setExercises] = useState(['Puxada frontal', 'Remada unilateral', 'Supino', 'Voador'])
+
+    const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+    const handleOpenExerciseDetails = () => {
+        navigation.navigate('exercise');
+    }
 
     return(
         <VStack flex={1}>
@@ -31,6 +39,7 @@ export const Home = () => {
                 _contentContainerStyle={{px:8}}
                 my={10}
                 maxH={10}
+                minH={10}
             />
             <VStack flex={1} px={8}>
                 <HStack justifyContent="space-between" mb={5}>
@@ -46,7 +55,9 @@ export const Home = () => {
                     data={exercises}
                     keyExtractor={item => item}
                     renderItem={({item}) => (
-                    <ExerciseCard/>
+                    <ExerciseCard
+                        onPress={handleOpenExerciseDetails}
+                    />
                     )}
                     showsVerticalScrollIndicator={false}
                     _contentContainerStyle={{
