ee034d3e1ed9a487ed218b9838adb61bb08ffb41
Johnny <johnnyduff25@gmail.com>
2023-07-31 22:02:36 -0300

    finalizando tela de perfil

--- jb_gym/src/screens/Profile.tsx
+++ jb_gym/src/screens/Profile.tsx
@@ -1,11 +1,82 @@
-import {Center,Text} from 'native-base';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
+import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
+import {Center, ScrollView, VStack, Skeleton, Text, Heading} from 'native-base';
+import {useState} from 'react';
+import { TouchableOpacity } from 'react-native';
+
+const PHOTO_SIZE = 33;
 
 export const Profile = () => {
+
+    const [photoIsLoading, setPhotoIsLoading] = useState(false);
     return(
-        <Center flex={1}>
-            <Text color="white">
-                Profile
-            </Text>
-        </Center>
+        <VStack flex={1}>
+            <ScreenHeader title="Perfil"/>
+            <ScrollView contentContainerStyle={{ paddingBottom: 36}}>
+                <Center mt={6} px={10}>
+                    {
+                        photoIsLoading ?
+                            <Skeleton 
+                                w={PHOTO_SIZE} 
+                                h={PHOTO_SIZE} 
+                                rounded="full"
+                                startColor="gray.500"
+                                endColor="gray.400"
+                            />
+                        :
+                            <UserPhoto
+                                source={{uri: 'https://github.com/jonathanalves25.png'}}
+                                alt="foto do usuario"
+                                size={PHOTO_SIZE}
+                            />
+
+                    }
+                    <TouchableOpacity>
+                        <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
+                            Altera foto    
+                        </Text>    
+                    </TouchableOpacity> 
+
+                    <Input 
+                        placeholder='Nome'
+                        bg="gray.600"
+                    /> 
+                    <Input 
+                        placeholder='E-mail'
+                        bg="gray.600"
+                        isDisabled
+                    />     
+                
+
+                    <Heading color="gray.200" fontSize="md" alignSelf="flex-start" mb={2} mt={12}>
+                        Alterar senha
+                    </Heading>
+                    <Input
+                        bg="gray.600"
+                        placeholder='Senha antiga'
+                        secureTextEntry
+                    />
+                    <Input
+                        bg="gray.600"
+                        placeholder='Nova senha'
+                        secureTextEntry
+                    />
+
+                    <Input
+                        bg="gray.600"
+                        placeholder='Confirme a nova senha'
+                        secureTextEntry
+                    />
+
+                    <Button 
+                        title="Atualizar"
+                        mb={4}
+                    />
+                </Center>
+
+            </ScrollView>
+        </VStack>
     )
 }
