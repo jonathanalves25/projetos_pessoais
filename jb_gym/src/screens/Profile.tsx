8c36ebc1cd7803cf21007d11a787ac71e0f28d1e
Johnny <johnnyduff25@gmail.com>
2023-08-01 22:10:00 -0300

    adicionado biblioteca para pegar imagem da galeria

--- jb_gym/src/screens/Profile.tsx
+++ jb_gym/src/screens/Profile.tsx
@@ -2,15 +2,52 @@ import { Button } from '@components/Button';
 import { Input } from '@components/Input';
 import { ScreenHeader } from '@components/ScreenHeader';
 import { UserPhoto } from '@components/UserPhoto';
-import {Center, ScrollView, VStack, Skeleton, Text, Heading} from 'native-base';
+import {Center, ScrollView, VStack, Skeleton, Text, Heading, useToast} from 'native-base';
 import {useState} from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
+import { FileInfo } from "expo-file-system";
 
 const PHOTO_SIZE = 33;
 
 export const Profile = () => {
 
     const [photoIsLoading, setPhotoIsLoading] = useState(false);
+    const [userPhoto, setUserPhoto] = useState('https://github.com/jonathanalves25.png');
+    const toast = useToast();
+
+    const handleUserPhotoSelect = async () => {
+        setPhotoIsLoading(true)
+        try {
+            const photoSelected = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                quality: 1,
+                aspect: [4, 4],
+                allowsEditing: true,
+                selectionLimit:1,
+            });
+            if (photoSelected.canceled) return;
+            
+            if (photoSelected.assets[0].uri) {
+                const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri) as FileInfo
+                if(photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+                    return toast.show({
+                        title: 'Essa imagem é muito grande, Escolhe uma de até 5MB.',
+                        placement: 'top',
+                        bgColor: 'red.500',
+                    })
+                }
+                setUserPhoto(photoSelected.assets[0].uri);
+            }
+            
+        } catch(error) {
+
+        } finally {
+            setPhotoIsLoading(false);
+        }
+
+    }
     return(
         <VStack flex={1}>
             <ScreenHeader title="Perfil"/>
@@ -27,15 +64,15 @@ export const Profile = () => {
                             />
                         :
                             <UserPhoto
-                                source={{uri: 'https://github.com/jonathanalves25.png'}}
+                                source={{uri: userPhoto}}
                                 alt="foto do usuario"
                                 size={PHOTO_SIZE}
                             />
 
                     }
-                    <TouchableOpacity>
+                    <TouchableOpacity onPress={handleUserPhotoSelect}>
                         <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
-                            Altera foto    
+                            Alterar foto    
                         </Text>    
                     </TouchableOpacity> 
 
