f48a86478bb06a6b8a210dca6f88e7f59ec1fffe
Jonathan Alves <50221540+jonathanalves25@users.noreply.github.com>
2024-06-06 10:53:22 -0300

    Delete portifolio_2023 directory

--- portifolio_2023/src/components/Title.jsx
+++
@@ -1,8 +0,0 @@
-
-export const Title = (props) => {
-  return (
-    <h2 className="text-red-700 font-sans font-bold text-xl">
-      {props.text}
-    </h2>
-  )
-}
--- portifolio_2023/src/components/TitleRole.jsx
+++
@@ -1,8 +0,0 @@
-
-export const TitleRole = (props) => {
-  return(
-    <h3 className="font-lg text-white font-sans uppercase py-4">
-      {props.text}
-    </h3>
-  )
-}
